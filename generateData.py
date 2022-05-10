17b8c7e554d83b11ccf315abd8fc1e5c4735984c
quechulo <aawangardowy@gmail.com>
2022-05-10 23:43:29 +0200

    generateData script improvement

--- generateData.py
+++ generateData.py
@@ -8,37 +8,39 @@ random.seed(0)
 
 def generateData(count=5, cor=False):
     data = []
-    licz = 1
+    licz = 2
     if cor is False:
         for x in range(0, count):
             tmp = []
             for j in range(1, 3):
-                tmp.append(licz+random.randint(0, 10))
+                tmp.append((licz+random.randint(-1, 8)) % 23)
                 licz = random.randint(1, 5)
             data.append(tmp)
     else:
         for x in range(0, count):
             tmp = []
-            for j in range(1, 3):
-                tmp.append(licz + random.randint(-1, 3))
-                licz += random.randint(-1, 3)
+
+            tmp.append(licz + random.randint(1, 3))
+            tmp.append(abs(licz + random.randint(-2, 3)))
+            licz += random.randint(1, 3)
+            licz = licz % 20
             data.append(tmp)
 
     df = pd.DataFrame(data)
     return df
 
 ### USAGE ###
-df = generateData(10, True)
+df = generateData(20, True)
 print(df)
 
 # Correlation between different variables
 #
-corr = df.corr()
+corr = df.corr(method='spearman')
 print(corr)
 #
 # Set up the matplotlib plot configuration
 #
-f, ax = plt.subplots(figsize=(10, 10))
+# f, ax = plt.subplots(figsize=(10, 10))
 #
 # Generate a mask for upper traingle
 #
